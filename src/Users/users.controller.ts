bd5eab64d44767cee5dc60879e344bcc251b592f
MJTwerda <mauriciotwerda@gmail.com>
2024-11-12 20:42:45 -0300

    MD respuestas de Users

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -3,12 +3,14 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
   Put,
   Query,
   Res,
+  UseFilters,
   UseGuards,
   UsePipes,
   ValidationPipe 
@@ -19,7 +21,10 @@ import { Response } from "express";
 import { BaseUserDto } from "./dtos/base-user-dto";
 import { CompleteUserDto } from './dtos/complete-user-dto';
 import { AuthGuard } from "src/Auth/guards/auth.guard";
+import { AllExceptionsFilter } from "src/commons/errorHandlers/allExceptionsFilter";
+import { HttpException } from '@nestjs/common';
 
+@UseFilters(AllExceptionsFilter)
 @Controller('users')
 export class UsersController { 
   constructor(
@@ -30,44 +35,26 @@ export class UsersController {
   @UsePipes(ValidationPipe)
   async createNewUser( 
     @Body() newUser: BaseUserDto, 
-    @Res() response: Response
   ) {
     const new_user_id = await this.usersService.createNewUser(newUser);
-    console.log({ new_user_id });
-
-    if (!new_user_id) {
-      return response.status(404).send(null);
-    };
-
-    return response.status(201).send({ new_user_id });
+    return { new_user_id };
   };
 
   @Get()
   @UseGuards(AuthGuard)
   async getUsersList(
     @Query() query: CommonQueryDto,
-    @Res() response: Response
   ) {
-    const users_list = await this.usersService.getUsersList(query);
-    return response.status(200).send(users_list);
+    return await this.usersService.getUsersList(query);
   };
 
   @Get(':userId')
   @UseGuards(AuthGuard)
   async getUserById( 
     @Param('userId') userId: string, 
-    @Res() response: Response
   ) {
-    if (!userId) {
-      return response.status(404).send(null);
-    };
-
     const founded_user = await this.usersService.getUserById(userId);
-
-    if (!founded_user) {
-      return response.status(404).send(null); // TODO: Agregar respuesta
-    };
-    return response.status(200).send(founded_user);
+    return { founded_user };
   };
 
   @Put()
@@ -75,28 +62,17 @@ export class UsersController {
   @UsePipes(new ValidationPipe({ forbidUnknownValues: true }))
   async updateUser(
     @Body() updatedUser: CompleteUserDto, 
-    @Res() response: Response
   ) {
     const updated_user_id = await this.usersService.updateUserInfo(updatedUser);
-
-    if (!updated_user_id) {
-      return response.status(404).send(null); // TODO: Agregar Respuesta
-    };
-
-    return response.status(200).send({ updated_user_id } );
+    return { updated_user_id };
   };
 
   @Delete(':userId')
   @UseGuards(AuthGuard)
   async deleteUserById(
-    @Param('userId') userId, 
-    @Res() response: Response 
+    @Param('userId') userId: string, 
   ) {
-    if (!userId) {
-      return response.status(400).send(null);
-    };
-
     const deleted_user_id = await this.usersService.deleteUserById(userId);
-    return response.status(201).send({ deleted_user_id });
+    return { deleted_user_id };
   };
 };
