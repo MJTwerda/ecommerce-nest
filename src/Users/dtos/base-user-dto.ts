b5c86f12316671580d7bd715f6941c00686a0227
MJTwerda <mauriciotwerda@gmail.com>
2024-11-12 20:45:43 -0300

    MD BaseUser DTO

--- src/Users/dtos/base-user-dto.ts
+++ src/Users/dtos/base-user-dto.ts
@@ -8,19 +8,21 @@ export class BaseUserDto {
     email: string;
 
   @IsString()
-  @MinLength(3)
+  @MinLength(3, { message: 'Name must be at least 3 characters long' })
   @MaxLength(50)
     name: string;
 
   @IsString()
-  // @MinLength(8, { message: 'Password must be at least 8 characters long' }) // Longitud mínima de 8 caracteres
-  @MaxLength(20, { message: 'Password must not exceed 20 characters' }) // Longitud máxima de 16 caracteres
-  // @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/, {
-  //   message: 'Password must contain at least one letter, one number, and one special character',
-  // })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(15, { message: 'Password must not exceed 20 characters' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$/, {
+    message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character: !@#$%^&*',
+  })
     password: string;
 
   @IsString()
+  @MinLength(3, { message: 'Address must be at least 3 characters long' })
+  @MaxLength(50, { message: 'Address must not exceed 50 characters' })
     address: string;
 
   @IsString()
@@ -28,11 +30,13 @@ export class BaseUserDto {
 
   @IsString()
   @IsOptional()
+  @MinLength(3, { message: 'Country must be at least 3 characters long' })
   @MaxLength(50, { message: 'Country must not exceed 50 characters' })
     country: string;
 
   @IsString()
   @IsOptional()
+  @MinLength(3, { message: 'City must be at least 3 characters long' })
   @MaxLength(50, { message: 'City must not exceed 50 characters' })
     city: string;
 }
